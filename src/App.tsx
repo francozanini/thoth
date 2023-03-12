755cfcddb0bae49ce18887dbd28d3529949ed885
franco.zanini <francozanini1995@gmail.com>
2023-03-12 20:01:07 +0100

    added eslint and tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import {useState} from "react";
-import {invoke} from "@tauri-apps/api/tauri";
+import { useState } from "react";
+import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
 type DesktopEntry = {
-    name: string,
-    icon: string,
-    exec: string,
-}
+  name: string;
+  icon: string;
+  exec: string;
+};
 
 function App() {
-    const [apps, setApps] = useState<ReadonlyArray<DesktopEntry>>([]);
+  const [apps, setApps] = useState<ReadonlyArray<DesktopEntry>>([]);
 
   async function search(searchInput: string) {
-      setApps(await invoke('all_apps', { searchInput}))
+    setApps(await invoke("all_apps", { searchInput }));
   }
 
   return (
     <div className="container">
-        <input type='text' onInput={event => search(event.currentTarget.value)} />
-        <ul>{apps.map(app => <li key={app.name}>{app.name}</li>)}</ul>
+      <input
+        type="text"
+        onInput={(event) => search(event.currentTarget.value)}
+      />
+      <ul>
+        {apps.map((app) => (
+          <li key={app.name}>{app.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
