46fe6f5368711e8f247ae68a5af752f3049333d2
Franco Zanini <francozanini1995@gmail.com>
2023-07-29 19:12:13 +0200

    (refactor) move things around until they make more sense

--- src/App.tsx
+++ src/App.tsx
@@ -1,126 +1,9 @@
 import React, { useEffect, useLayoutEffect, useState } from "react";
-import { invoke } from "@tauri-apps/api/tauri";
 import { appWindow, LogicalSize } from "@tauri-apps/api/window";
-
-type Runnable = {
-  name: string;
-  file_name?: string;
-  icon?: string;
-  exec: string;
-};
-
-function useAutoWindowResizing(dependencies: React.DependencyList) {
-  useLayoutEffect(() => {
-    async function resizeWindow() {
-      const height = document.getElementById("container")?.clientHeight ?? 0;
-      await appWindow.setSize(new LogicalSize(750, height));
-    }
-
-    resizeWindow().catch(console.error);
-  }, dependencies);
-}
-
-function useListNavigation(listLength: number) {
-  const [selectedIndex, moveIndexTo] = useState(0);
-  const keyboardHandler = (event: KeyboardEvent) => {
-    if (event?.code === "ArrowDown" || event.code === "Tab") {
-      event.preventDefault();
-      if (selectedIndex === listLength - 1) {
-        moveIndexTo(0);
-      } else {
-        moveIndexTo(Math.min(selectedIndex + 1, listLength - 1));
-      }
-    }
-
-    if (event?.code === "ArrowUp") {
-      if (selectedIndex === 0) {
-        moveIndexTo(listLength - 1);
-      } else {
-        moveIndexTo(Math.max(selectedIndex - 1, 0));
-      }
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener("keydown", keyboardHandler);
-    return () => window.removeEventListener("keydown", keyboardHandler);
-  });
-
-  return selectedIndex;
-}
-
-async function runCommand(
-  command: string,
-  afterRun: (message: string) => void
-) {
-  const wasSuccessful = await invoke("run", { path: command });
-  const inputElement: HTMLInputElement = document.getElementById(
-    "search-bar"
-  )! as HTMLInputElement;
-  inputElement.value = "";
-
-  if (wasSuccessful) {
-    afterRun("thoth.app");
-  } else {
-    afterRun("Error running command");
-  }
-}
-
-function raise(message: string): never {
-  throw new Error(message);
-}
-
-function SearchResults({
-  runnables,
-  onCommand,
-}: {
-  runnables: Runnable[];
-  onCommand: (message: string) => void;
-}) {
-  const selectedIndex = useListNavigation(runnables.length);
-  const handleEnterKey = (event: KeyboardEvent) => {
-    if (event.code === "Enter") {
-      runCommand(runnables[selectedIndex].exec, onCommand);
-      invoke("hide_window", {});
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener("keydown", handleEnterKey);
-    return () => document.removeEventListener("keydown", handleEnterKey);
-  });
-
-  if (!runnables.length) {
-    return null;
-  }
-
-  return (
-    <ul className="mt-2 px-2">
-      {runnables.map((app, index) => (
-        <li
-          className={cn(
-            "cursor-pointer",
-            index === selectedIndex && "bg-gray-400"
-          )}
-          key={app.file_name}
-        >
-          <button
-            id={index === 0 ? "first-search-result" : ""}
-            className="w-full text-left focus:bg-gray-400 focus:outline-none"
-            type="button"
-            onClick={() => runCommand(app.exec, onCommand)}
-          >
-            {app.name}
-          </button>
-        </li>
-      ))}
-    </ul>
-  );
-}
-
-function cn(...classes: (string | boolean)[]): string {
-  return classes.filter(Boolean).join(" ");
-}
+import { SearchResults } from "./SearchResults";
+import { SearchBar } from "./SearchBar";
+import { Runnable, cn } from "./things";
+import { invoke } from "@tauri-apps/api/tauri";
 
 function Footer({
   includeBorder,
@@ -141,50 +24,35 @@ function Footer({
   );
 }
 
-function SearchBar({ onSearch }: { onSearch: (result: Runnable[]) => void }) {
-  async function search(searchInput: string) {
-    const retrieved = await invoke<Runnable[]>("search", {
-      searchInput,
-    });
-    onSearch(retrieved);
-  }
-
-  const preventArrowMovement = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.code === "ArrowUp" || e.code === "ArrowDown") {
-      e.preventDefault();
-    }
-  };
-
-  return (
-    <input
-      id="search-bar"
-      autoFocus
-      type="text"
-      className="w-full rounded-t-xl border-b-2 border-solid border-gray-300 bg-gray-200 p-2 pt-4 focus:outline-none"
-      placeholder="Search for apps or commands..."
-      onKeyDown={preventArrowMovement}
-      onChange={(event) => search(event.target.value)}
-    />
-  );
-}
-
 function App() {
   const [runnables, setRunnables] = useState<Runnable[]>([]);
   const [footerMessage, setFooterMessage] = useState<string>("thoth.app");
-  useAutoWindowResizing([runnables]);
+  const mainElementId = "main-element";
+  useLayoutEffect(() => {
+    function resizeWindow() {
+      const height = document.getElementById(mainElementId)?.clientHeight ?? 0;
+      return appWindow.setSize(new LogicalSize(750, height));
+    }
+
+    resizeWindow().catch(console.error);
+  }, [runnables]);
 
   return (
-    <div id="container" className="h-full rounded-xl bg-gray-200 font-mono">
+    <main
+      id={mainElementId}
+      className="h-full rounded-xl bg-gray-200 font-mono"
+    >
       <SearchBar onSearch={setRunnables} />
       <SearchResults
         runnables={runnables}
         onCommand={(message) => {
           setFooterMessage(message);
           setRunnables([]);
+          invoke("hide_window", {});
         }}
       />
       <Footer message={footerMessage} includeBorder={runnables.length > 0} />
-    </div>
+    </main>
   );
 }
 
