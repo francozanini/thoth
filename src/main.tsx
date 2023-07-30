dfb832e53cc35357075833f1101d80a09d2725ef
Franco Zanini <francozanini1995@gmail.com>
2023-07-30 16:55:32 +0200

    unregister commands on startup to avoid hmr issues

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { register } from "@tauri-apps/api/globalShortcut";
+import {
+  register,
+  isRegistered,
+  unregister,
+} from "@tauri-apps/api/globalShortcut";
 import { invoke } from "@tauri-apps/api";
 
 document.addEventListener("keydown", (event) => {
@@ -11,13 +15,23 @@ document.addEventListener("keydown", (event) => {
   }
 });
 
-await register("Control+Shift+Space", async () => {
-  if (document.hasFocus()) {
-    await invoke("hide_window", {});
-  } else {
-    await invoke("show_window", {});
+async function init() {
+  await unregister("Control+Shift+Space");
+
+  if (await isRegistered("Control+Shift+Space")) {
+    return;
   }
-});
+
+  register("Control+Shift+Space", async () => {
+    if (document.hasFocus()) {
+      await invoke("hide_window", {});
+    } else {
+      await invoke("show_window", {});
+    }
+  });
+}
+
+await init();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
